74d9f616e12c738d146182128b5fcb68a8104657
Ciprian47 <ciprian.i.dumitrescu@gmail.com>
2017-01-02 15:23:12 +0200

    paginatie_begin - exemplu cu tot cu filtru - trebuie imbunatatit

--- src/main/webapp/resources/js/controllers/MainController.js
+++ src/main/webapp/resources/js/controllers/MainController.js
@@ -5,7 +5,7 @@
 * @constructor
 * Celelalte controllere sunt accesate prin $routeProvider. Doar asta este accesat din ng-controller.
 */
-var MainController = function ($scope, $rootScope, $http, i18n, $location) {
+var MainController = function ($scope, $rootScope, $http, i18n, $location, $filter) {
     $scope.language = function () {
         return i18n.language;
     };
@@ -30,5 +30,42 @@ var MainController = function ($scope, $rootScope, $http, i18n, $location) {
         $scope.$emit('event:logoutRequest');
         $location.url('/action/cars');
     };
+
+    //inceput paginatie
+    $scope.currentPage = 0;
+    $scope.pageSize = 10;
+    $scope.data = [];
+    $scope.q = '';
+
+    $scope.getData = function () {
+        // needed for the pagination calc
+        // https://docs.angularjs.org/api/ng/filter/filter
+        return $filter('filter')($scope.data, $scope.q);
+        /*
+         // manual filter
+         // if u used this, remove the filter from html, remove above line and replace data with getData()
+
+         var arr = [];
+         if($scope.q == '') {
+         arr = $scope.data;
+         } else {
+         for(var ea in $scope.data) {
+         if($scope.data[ea].indexOf($scope.q) > -1) {
+         arr.push( $scope.data[ea] );
+         }
+         }
+         }
+         return arr;
+         */
+    };
+
+    $scope.numberOfPages=function(){
+        return Math.ceil($scope.getData().length/$scope.pageSize);
+    };
+
+    for (var i=0; i<65; i++) {
+        $scope.data.push("Item "+i);
+    }
+    //sfarsit paginatie
 
 };
