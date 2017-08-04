3e549b1d8b9040bbe09cdcd9766586dba435b211
CiprianDumi <ciprian.dumitrescu@infrasoft.ro>
2017-08-04 21:50:30 +0300

    Safe save

--- src/main/java/com/navras/springmvcangularjs/database/OracleCon.java
+++ src/main/java/com/navras/springmvcangularjs/database/OracleCon.java
@@ -22,77 +22,69 @@ public class OracleCon{
 //    }
 
     public static void main(String args[]){
-        OracleCon.getSql("select * from prima where id=2", "id,text");
+        OracleCon.getSql("select * from prima", "id,text", "GET");
 
     }
 
-    public static void getSql(String sql, String column) {
+    public static void getSql(String sql, String column, String action) {
         Connection con = null;
         HashMap<String, String> resp = new HashMap<>();
         String multiCols[] = new String[1];
         int cont = 0;
         if (!column.trim().isEmpty() && !column.trim().equals("") && !column.contains("*")) {
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "cld", "cld");
-            Statement stmt = con.createStatement();
+            try {
+                //DB initialization
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "cld", "cld");
+                Statement stmt = con.createStatement();
 
-            ResultSet rs = stmt.executeQuery(sql);
+                //executing sql statement
+                ResultSet rs = stmt.executeQuery(sql);
 
-            ResultSetMetaData rsCols = rs.getMetaData();
-            int numberOfCols = rsCols.getColumnCount();
-            int comaNumber = 1;
+                //geting result set column names
+                ResultSetMetaData rsCols = rs.getMetaData();
+                int numberOfCols = rsCols.getColumnCount();
+                int comaNumber = 1;
 
-            if (column.contains(",")) {
-                comaNumber = column.split(",").length;
-                multiCols = column.split(",");
-            }else{
-                multiCols[0]=column;
-            }
+                if (column.contains(",")) {
+                    comaNumber = column.split(",").length;
+                    multiCols = column.split(",");
 
-            while (rs.next()) {
+                }else{
+                    multiCols[0]=column;
 
-                for (cont = 1; cont <= numberOfCols; cont++) {
+                }
 
-                    String numeCol = rsCols.getColumnName(cont);
+                while (rs.next()) {
+                    for (cont = 1; cont <= numberOfCols; cont++) {
+                        String numeCol = rsCols.getColumnName(cont);
+                        int i = 0;
 
-                    int i = 0;
-                    for (i = 0; i < comaNumber; i++){
+                        for (i = 0; i < comaNumber; i++){
+                            if (numeCol.toUpperCase().equals(multiCols[i].toUpperCase())) {
 
-                        if (numeCol.toUpperCase().equals(multiCols[i].toUpperCase())) {
+                                if (multiCols[i] != null && rs.getString(numeCol) != null){
+                                    resp.put(multiCols[i],rs.getString(numeCol));
 
-                            if (multiCols[i] != null && rs.getString(numeCol) != null){
-                                resp.put(multiCols[i],rs.getString(numeCol));
+                                }
                             }
-
-
-
-
-//                        return rs.getString(numeCol);
                         }
-
                     }
                 }
-            }
-            System.out.println(resp);
+                System.out.println(resp);
 
-
-//step5 close the connection object
-//            con.close();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        } finally {
-            try {
-                if (con != null) {
-                    con.close();
-                }
             } catch (Exception e) {
-//                logger.error("Canot close db connection: " + e);
                 System.out.println(e);
+            } finally {
+                try {
+                    if (con != null) {
+                        con.close();
+                    }
+                } catch (Exception e) {
+    //                logger.error("Canot close db connection: " + e);
+                    System.out.println(e);
+                }
             }
         }
-    }
-
     }
 }
